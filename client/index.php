bb6b8c07434547f40ac85212c6835d0ec25bcb19
Patrick Ohnewein <patrick.ohnewein@lugbz.org>
2015-01-02 18:16:29 +0100

    ficed an error in the previous fix of #12
    Conflicts:
    	client/index.php
    	client/js/sasabus.js

--- client/index.php
+++ client/index.php
@@ -19,53 +19,55 @@
 ?>
 <!DOCTYPE html>
 <html dir="ltr">
-	<head>
-		<meta charset="utf-8" />
-		<title><?php ___("meranotitle"); ?></title>
-		<link rel="icon" href="favicon.ico" type="image/x-icon"> 
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-		<!--[if IE]>
-			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-
-		<!-- Stylesheets -->
-		<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/ui-lightness/jquery-ui.css" type="text/css" media="all" />
-		<link href="css/standard.css" rel="stylesheet" media="screen,projection,print" type="text/css" />
-		<link href="css/smartphone.css" media="only screen and (max-width: 671px)" rel="stylesheet" /> <!-- max-device-width: 671px -->
-		<!--<link href="css/smartphoneLandscape.css" media="only screen and (max-width: 671px) and (orientation: landscape)" rel="stylesheet" /> --> <!-- max-device-width: 671px -->
-		<!-- media="only screen and (orientation: landscape) -->
-		<script src="js/detect_device.js" type="text/javascript"></script>
-		<script src="js/OpenLayers/OpenLayers.sasabus.js"></script>
-		<!--<script src="http://sasatest.r3-gis.com/js/OpenLayers/lib/OpenLayers.js"></script>-->
-		<script src="js/OpenLayers/proj4js.min.js"></script>
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-		<script src="js/sasabus.js"></script>
-		<script> var txtVariante = '<?php ___("varianti"); ?>';</script>
-
-		<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/ui-lightness/jquery-ui.css" type="text/css" media="all" />
-		<style>
-			.ui-dialog {z-index:1000;}
-		</style>
-		<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
-		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<meta name="apple-mobile-web-app-status-bar-style" content="default" /> 
-	</head>
-	<!--[if IE 8]>
-	<body class="lte-8 ie-8 map-body">
-	<![endif]-->
-	<!--[if lte IE 7]>
-	<body class="lte-8 lte-7 map-body">
-	<![endif]-->
-	<!--[if gt IE 8]>
-	<body class="map-body ie-9">
-	<![endif]-->
-	<!--[if !IE]><!-->
-	<body class="map-body <?php echo $lang ?>">
-		<script>  
-		if (/*@cc_on!@*/false) {  
-			document.documentElement.className+=' ie10';  
-		}  
+    <head>
+        <meta charset="utf-8" />
+        <title><?php ___("meranotitle"); ?></title>
+        <link rel="icon" href="favicon.ico" type="image/x-icon"> 
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <!--[if IE]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/ui-lightness/jquery-ui.css" type="text/css" media="all" />
+        <link href="css/standard.css" rel="stylesheet" media="screen,projection,print" type="text/css" />
+       	<link href="css/smartphone.css" media="only screen and (max-width: 671px)" rel="stylesheet" /> <!-- max-device-width: 671px -->
+        <!--<link href="css/smartphoneLandscape.css" media="only screen and (max-width: 671px) and (orientation: landscape)" rel="stylesheet" /> --> <!-- max-device-width: 671px -->
+        <!-- media="only screen and (orientation: landscape) -->
+	<script src="js/detect_device.js" type="text/javascript"></script>
+        <script src="js/OpenLayers/OpenLayers.sasabus.js"></script>
+        <!--<script src="http://sasatest.r3-gis.com/js/OpenLayers/lib/OpenLayers.js"></script>-->
+        <script src="js/OpenLayers/proj4js.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+        <script src="js/sasabus.js"></script>
+        <script>  
+			var txtVariante = '<?php ___("varianti"); ?>';
+		</script>
+		
+        <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/ui-lightness/jquery-ui.css" type="text/css" media="all" />
+        <style>
+        	.ui-dialog {z-index:1000;}
+        </style>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" /> 
+    </head>
+    <!--[if IE 8]>
+        <body class="lte-8 ie-8 map-body">
+    <![endif]-->
+    <!--[if lte IE 7]>
+        <body class="lte-8 lte-7 map-body">
+    <![endif]-->
+    <!--[if gt IE 8]>
+        <body class="map-body ie-9">
+    <![endif]-->
+    <!--[if !IE]><!-->
+        <body class="map-body <?php echo $lang ?>">
+        <script>  
+			if (/*@cc_on!@*/false) {  
+				document.documentElement.className+=' ie10';  
+			}  
 		</script>
 	<!--<![endif]-->
 		<div class="map-container" id="map" style="width:100%;height:100%">
@@ -141,39 +143,36 @@
 						</div>
 					</div>
 				</div>
-			</main>
-			<footer id="footer-1" class="footer">
-				<ul class="logos">
-					<li class="merano"><a target="_blank" href="<?php ___('linkmerano'); ?>"><?php ___('merano'); ?></a></li>
-					<li class="tis"><a target="_blank" href="<?php ___('linktis'); ?>">TIS innovation park South Tyrol</a></li>
-					<li class="sasa"><a target="_blank" href="<?php ___('linksasa'); ?>">SASA Bus</a></li>
-				</ul>
-			</footer>
-			<footer id="footer-2" class="footer">
-				<nav>
-					<?php ___('infocontatti'); ?><a href="mailto:info@meran.eu">info@meran.eu</a>
-					<!--<ul>
-						<li><a href="#servizio"><?php ___('servizio'); ?></a></li>
-						<li><a href="#impressum"><?php ___('impressum'); ?></a></li>
-						<li><a href="#contatti"><?php ___('contatti'); ?></a></li>
-					</ul>-->
-				</nav>
-				<div id="pages">
-					<div id="servizio" class="page">
-						<h3><?php ___('servizio'); ?></h3>
-					</div>
-					<div id="impressum" class="page">
-						<h3><?php ___('impressum'); ?></h3>
-					</div>
-					<div id="contatti" class="page">
-						<h3><?php ___('contatti'); ?></h3>
-					</div>
-				</div>
-			</footer>
-		</div>
-
-	        <p id="credits"><?php ___('mappe'); ?> <a href="http://www.openstreetmap.org/copyright" rel="external">OpenStreetMap</a> &amp; <a href="<?php ___('linkr3gis'); ?>" rel="external">R3 GIS</a> - <?php ___('design'); ?> <a href="<?php ___('linkmadeincima'); ?>" rel="external">Madeincima</a></p>
-            
+	            </main>
+	            <footer id="footer-1" class="footer">
+	                <ul class="logos">
+	                    <li class="merano"><a target="_blank" href="<?php ___('linkmerano'); ?>"><?php ___('merano'); ?></a></li>
+	                    <li class="tis"><a target="_blank" href="<?php ___('linktis'); ?>">TIS innovation park South Tyrol</a></li>
+	                    <li class="sasa"><a target="_blank" href="<?php ___('linksasa'); ?>">SASA Bus</a></li>
+	                </ul>
+	            </footer>
+	            <footer id="footer-2" class="footer">
+	            	<nav>
+                    	<?php ___('infocontatti'); ?><a href="mailto:info@meran.eu">info@meran.eu</a>
+	            		<!--<ul>
+	            			<li><a href="#servizio"><?php ___('servizio'); ?></a></li>
+	            			<li><a href="#impressum"><?php ___('impressum'); ?></a></li>
+	            			<li><a href="#contatti"><?php ___('contatti'); ?></a></li>
+	            		</ul>-->
+	            	</nav>
+	            	<div id="pages">
+	            		<div id="servizio" class="page">
+		            			<h3><?php ___('servizio'); ?></h3>
+		            	</div>
+	            		<div id="impressum" class="page">
+		            			<h3><?php ___('impressum'); ?></h3>
+	            		</div>
+	            		<div id="contatti" class="page">
+		            			<h3><?php ___('contatti'); ?></h3>
+	            		</div>
+	            	</div>
+	            </footer>
+            </div>
 		<div id="busPopup" style="display:none;">
 			<div class="clearfix pophead">
 				<h2 class="bus c-${lidname}"><?php ___('linea'); ?> ${lidname}</h2>
